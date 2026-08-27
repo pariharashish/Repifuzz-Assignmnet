87717cb2603836f61989d1803e5cfb663cc6f228
Ashish <ashishkumarsingh2u@gmail.com>
2026-08-28 04:40:00 +0530

    incident lifecycle

---
+++ src/main/java/com/repifuzz/Entity/AuditAction.java
@@ -0,0 +1,14 @@
+package com.repifuzz.Entity;
+
+public enum AuditAction {
+    CREATED,
+    STATUS_CHANGED,
+    SEVERITY_ASSIGNED,
+    ASSIGNED,
+    UNASSIGNED,
+    COMMENTED,
+    RESOLVED,
+    REOPENED,
+    CLOSED,
+    MERGED_DUPLICATE
+}
